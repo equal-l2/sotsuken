08781f21d791ae5835211c93421e3e53851ec3d4
equal-l2 <eng.equall2@gmail.com>
2020-01-27 20:27:14 +0900

    iface: use Vue

--- iface/main.js
+++ iface/main.js
@@ -1,28 +1,62 @@
-let src_cont = document.getElementById("src-cont");
-let var_cont = document.getElementById("var-cont");
-let cnt = 0
-let line = 0;
-let trace_content;
+let app = new Vue({
+    el: "#app",
+
+    data: {
+        cnt: 0,
+        max_cnt: 0,
+        line: 0,
+        trace_content: null,
+    },
+
+    methods: {
+        nextStep: () => {
+            app.cnt += 1;
+            move_step();
+        },
+        prevStep: () => {
+            app.cnt -= 1;
+            move_step();
+        },
+        readSrc: evn => {
+            let f = evn.target.files[0];
+            let rdr = new FileReader()
+            rdr.readAsText(f);
+            rdr.addEventListener("load", e => {
+                try {
+                    app.trace_content = JSON.parse(rdr.result);
+                } catch (e) {
+                    alert("Invalid file, cannot parse as JSON");
+                    return;
+                }
+                init_src_view();
+            });
+        }
+    }
+});
 
 function init_src_view() {
+    let src_cont = document.getElementById("src-cont");
     src_cont.innerHTML = ""
-    cnt = 0;
-    for (s of trace_content.src) {
+    app.cnt = 0;
+    for (s of app.trace_content.src) {
         let li = document.createElement("li");
         li.appendChild(document.createTextNode(s));
         src_cont.appendChild(li);
     }
+    app.max_cnt = app.trace_content.steps.length;
     move_step()
 }
 
 function move_step() {
-    let new_line = trace_content.steps[cnt].loc.lineNumber;
-    src_cont.childNodes[line].className = src_cont.childNodes[line].className.replace(/selected/, "");
+    let var_cont = document.getElementById("var-cont");
+    let new_line = app.trace_content.steps[app.cnt].loc.lineNumber;
+    let src_cont = document.getElementById("src-cont");
+    src_cont.childNodes[app.line].className = src_cont.childNodes[app.line].className.replace(/selected/, "");
     src_cont.childNodes[new_line].className = src_cont.childNodes[new_line].className + " selected";
-    line = new_line;
+    app.line = new_line;
 
     var_cont.innerHTML = "";
-    let vars = trace_content.steps[cnt].vars;
+    let vars = app.trace_content.steps[app.cnt].vars;
     for ([k, v] of Object.entries(vars)) {
         let li = document.createElement("li");
         let ul_nest = document.createElement("ul");
@@ -31,44 +65,9 @@ function move_step() {
         li.appendChild(ul_nest);
         for (c of v) {
             let li_nest = document.createElement("li");
-            console.log(c.name);
+            //console.log(c.name);
             li_nest.appendChild(document.createTextNode(c.name + ":" + c.value.value));
             ul_nest.appendChild(li_nest);
         }
     }
-}
-
-{
-    let trace_read = document.getElementById("src-read");
-    trace_read.addEventListener("change", function(e) {
-        let f = e.target.files[0];
-        let rdr = new FileReader()
-        rdr.readAsText(f);
-        rdr.addEventListener("load", e => {
-            try {
-                trace_content = JSON.parse(rdr.result);
-            } catch (e) {
-                alert("Invalid file, cannot parse as JSON");
-            }
-            init_src_view();
-        });
-    });
-}
-
-{
-    let next_button = document.getElementById("next");
-    next_button.addEventListener("click", e => {
-        cnt += 1;
-        console.log(line);
-        move_step();
-    });
-}
-
-{
-    let prev_button = document.getElementById("prev");
-    prev_button.addEventListener("click", e => {
-        cnt -= 1;
-        console.log(line);
-        move_step();
-    });
 }
